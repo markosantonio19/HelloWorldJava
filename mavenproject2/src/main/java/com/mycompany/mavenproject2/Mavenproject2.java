d4dc0db5859baa892915bd15dfb7575c3e5627f8
markosantonio19 <1433811@senati.pe>
2023-08-17 11:49:11 -0500

    agrege comentarios

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -13,25 +13,31 @@ import java.util.Scanner;
 public class Mavenproject2 {
 
     public static void main(String[] args) {
-        
+        /*AGREGE TITUTO*/
         System.out.println("CALCULADORA");
+        
+        /*AGREGE SCANER*/
         Scanner marko = new Scanner(System.in);
         System.out.println("ingrese primer numero");
         int numero1 = marko.nextInt();
         System.out.println("ingrese segundo  numero");
         int numero2 = marko.nextInt();
         
+        /*OPERACIONES*/
         int suma = numero1+numero2;
         int resta = numero1-numero2;
         int multiplicacion = numero1*numero2;
         int divicion = numero1/numero2;
         
+        
+        /*MUESTRAS*/
         System.out.println("la suma es: "+ suma);
         System.out.println("la resta es"+ resta);
         System.out.println("la multiplicacion es "+ multiplicacion);
         System.out.println("la divicion es "+ divicion);
         
-        System.out.println("progrsamado por markitos");
+        
+        System.out.println("programado por markitos");
              
         
         
