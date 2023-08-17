c3c3beaf4a340c614e49e353fcc11bf1159b8cf9
markosantonio19 <1433811@senati.pe>
2023-08-17 09:49:37 -0500

    creamos unca suma y resta

---
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        Scanner marko = new Scanner(System.in);
+        System.out.println("ingrese primer numero");
+        int numero1 = marko.nextInt();
+        System.out.println("ingrese segundo  numero");
+        int numero2 = marko.nextInt();
+        
+        int suma = numero1+numero2;
+        int resta = numero1-numero2;
+        
+        System.out.println("la suma es: "+ suma);
+        System.out.println("la resta es"+ resta);
+        
+    }
+}
