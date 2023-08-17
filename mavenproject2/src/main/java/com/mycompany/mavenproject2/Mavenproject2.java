6ed080c57489cff0de020893b6014a3915c8016a
markosantonio19 <1433811@senati.pe>
2023-08-17 09:59:34 -0500

    agrege multiplicacion

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -21,9 +21,13 @@ public class Mavenproject2 {
         
         int suma = numero1+numero2;
         int resta = numero1-numero2;
+        int multiplicacion = numero1*numero2;
         
         System.out.println("la suma es: "+ suma);
         System.out.println("la resta es"+ resta);
+        System.out.println("la multiplicacion es "+ multiplicacion);
+             
+        
         
     }
 }
