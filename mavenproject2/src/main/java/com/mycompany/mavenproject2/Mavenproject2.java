7576be326af24eaac132d212e812abd656b525f3
markosantonio19 <1433811@senati.pe>
2023-08-17 10:05:10 -0500

    agrege divicion

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -22,10 +22,12 @@ public class Mavenproject2 {
         int suma = numero1+numero2;
         int resta = numero1-numero2;
         int multiplicacion = numero1*numero2;
+        int divicion = numero1/numero2;
         
         System.out.println("la suma es: "+ suma);
         System.out.println("la resta es"+ resta);
         System.out.println("la multiplicacion es "+ multiplicacion);
+        System.out.println("la divicion es "+ divicion);
              
         
         
